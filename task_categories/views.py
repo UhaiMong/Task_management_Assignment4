0bce76fe57a677222b05571209af88b4be02e5ed
UhaiMong <uhaimarma2016@gmail.com>
2024-01-17 23:47:45 +0600

    all done

--- task_categories/views.py
+++ task_categories/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import CategoryForm
 
 # Create your views here.
 
 
 def AddCategory(request):
-    cats = CategoryForm()
+    if request.method == 'POST':
+        cats = CategoryForm(request.POST)
+        if cats.is_valid():
+            cats.save()
+            redirect('homepage')
+    else:
+        cats = CategoryForm()
     return render(request, 'task_categories.html', {'form': cats})
